acd7a0cb35a9221f502bc6edea5e6d0bf330537f
Bragren <bragren17@gmail.com>
2022-07-15 15:14:50 -0600

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 2, 1),
+    ('Janet', 'Hawks', 1, null),
+    ('Robert', 'Thompson', 3, null),
+    ('Maria', 'Tyler', 1, null);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Computer Engineer', 90000, 3),
+    ('Lawyer', 180000, 4),
+    ('Sales Associate', 60000, 1),
+    ('HR Representative', 80000, 2);
+
+20 lines (18 sloc)  494 Bytes
+
+INSERT INTO departments (name)
+VALUES
+    ('Engineering'),
+    ('Sales'),
+    ('HR'),
+    ('Legal');
